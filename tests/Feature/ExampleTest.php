2de79422a2dee1c05723556446ff522b3446e672
Ashwani Kumar <ashwani.cotocus@gmail.com>
2025-08-21 02:55:56 +0530

    adding

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,10 +3,13 @@
 namespace Tests\Feature;
 
 use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function test_home_is_ok_for_authenticated_user(): void
     {
         $user = User::factory()->create();
